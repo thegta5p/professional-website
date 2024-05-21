e94cf1aa79ce105fa7915692e0d3fce7f26479c8
Isaac Castro <icast065@ucr.edu>
2024-05-21 01:05:19 -0700

    added in data for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,22 @@
+"use client";
 import { title } from "@/components/primitives";
 import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Image} from "@nextui-org/react";
+import React from "react";
 
 
 import AboutData from "../../data/about.json";
 export default function AboutPage() {
+
+  const {isOpen, onOpen, onClose} = useDisclosure();
+  const [content, setContent] = React.useState("test");
+  const [title, setTitle] = React.useState("title");
+
+  const handleOpen = (content :string, title :string) => {
+    setContent(content);
+    setTitle(title);
+    onOpen();
+  }
 
   
   return (
@@ -12,16 +24,28 @@ export default function AboutPage() {
       <div>
         {AboutData.map((data) => (
           <Card key={data.title}>
-            <CardHeader>
+            <CardHeader className="absolute top-1">
               <h1>{data.title}</h1>
             </CardHeader>
             <Image src={data.image} alt="background image" className="z-0 w-full h-full object-cover"/>
-            <CardFooter>
-              <Button key={data.title}>Read More</Button>
+            <CardFooter className="absolute bottom-0">
+              <Button key={data.title} onPress={()=> handleOpen(data.content, data.title)}>Read More</Button>
             </CardFooter>
           </Card>
         ))}
       </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>{title}</ModalHeader>
+              <ModalBody>
+                <p>{content}</p>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }

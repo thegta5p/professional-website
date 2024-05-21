305072bd94bae540ca44631aeaf65a723c6e0147
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2024-05-21 00:03:06 -0700

    created data for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,27 @@
 import { title } from "@/components/primitives";
+import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Image} from "@nextui-org/react";
 
+
+import AboutData from "../../data/about.json";
 export default function AboutPage() {
+
+  
   return (
     <div>
-      <h1 className={title()}>About</h1>
+      <div>
+        {AboutData.map((data) => (
+          <Card key={data.title}>
+            <CardHeader>
+              <h1>{data.title}</h1>
+            </CardHeader>
+            <Image src={data.image} alt="background image" className="z-0 w-full h-full object-cover"/>
+            <CardFooter>
+              <Button key={data.title}>Read More</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }

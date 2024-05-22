6177dc355f9770424658d727b5b38ffab76e122b
Isaac Castro <icast065@ucr.edu>
2024-05-22 00:32:25 -0700

    added in education page

---
+++ components/education-info.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const EducationInfo = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-2">
+        <h1 className="text-4xl font-bold">
+          University Of California Riverside
+        </h1>
+        <p className="place-self-end">2020-2024</p>
+      </div>
+      <p>B.S In Computer Science</p>
+    </div>
+  );
+};
+
+export default EducationInfo;
--- config/site.ts
+++ config/site.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
     },
     {
       label: "Education/Skills",
-      href: "/docs",
+      href: "/education",
     },
     {
       label: "Experience",

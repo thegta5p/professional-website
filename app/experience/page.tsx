353b49ba6e212a8d2efbe1ff9c486b7da67ed814
Isaac Castro <icast065@ucr.edu>
2024-05-21 23:25:26 -0700

    formatted files

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -3,11 +3,16 @@ import ContentCard from "@/components/content-card";
 import ExperienceData from "../../data/experience.json";
 
 export default function ExperiencePage() {
-    return (
-        <div className="grid grid-rows-5 gap-12">
-         {ExperienceData.map((experience) => (
-                <ContentCard key={experience.id} title={experience.title} description={experience.description} date={experience.date} />
-        ))}
-        </div>
-    );
+  return (
+    <div className="grid grid-rows-5 gap-12">
+      {ExperienceData.map((experience) => (
+        <ContentCard
+          key={experience.id}
+          title={experience.title}
+          description={experience.description}
+          date={experience.date}
+        />
+      ))}
+    </div>
+  );
 }

1baa0fa28d0d26b92a47817b05a67910c2cfe05b
Isaac Castro <icast065@ucr.edu>
2024-05-22 22:49:21 -0700

    added in more information to the modal

--- app/big-flex/page.tsx
+++ app/big-flex/page.tsx
@@ -1,62 +1,90 @@
 "use client";
 import { title } from "@/components/primitives";
-import {Input, Button, Select, SelectItem} from "@nextui-org/react";
-import React, {useEffect} from "react";
+import { Input, Button, Select, SelectItem } from "@nextui-org/react";
+import React, { useEffect } from "react";
 import AnimeTiles from "@/components/anime-tiles";
 
 let baseApi = "https://kitsu.io/api/edge/";
 
 export default function BigFlex() {
-    const [value, setValue] = React.useState("");
-    const [filter, setFilter] = React.useState("");
-    const [animeListing, setAnimeListing] = React.useState([]);
+  const [value, setValue] = React.useState("");
+  const [filter, setFilter] = React.useState("");
+  const [animeListing, setAnimeListing] = React.useState([]);
 
-    const filterOptions = [
-        {
+  const filterOptions = [
+    {
+      value: "categories",
+      label: "Category",
+    },
+    {
+      value: "text",
+      label: "Anime Name",
+    },
+  ];
+  const handleSelectionChange = (e) => {
+    setFilter(e.target.value);
+  };
 
-            value: "categories",
-            label: "Category"
-        },
-        {
+  const handleRequest = async () => {
+    const url = `${baseApi}anime?filter[${filter}]=${value}`;
+    try {
+      const animeData = await fetch(url).then((animeData) => animeData.json());
+      console.log(animeData);
+      alert(animeData.data[0].attributes.canonicalTitle);
+      setAnimeListing(animeData.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-            value: "text",
-            label: "Anime Name"  
-        }
-    ];
-    const handleSelectionChange = (e) => {
-        setFilter(e.target.value);
-    };
-
-    const handleRequest = async () => {
-        const url = `${baseApi}anime?filter[${filter}]=${value}`;
-        try {
-            const animeData = await fetch(url).then((animeData) => animeData.json());
-            console.log(animeData);
-            alert(animeData.data[0].attributes.canonicalTitle);
-            setAnimeListing(animeData.data);
-           
-        }
-        catch (error) {
-            console.error(error);
-        }
-
-    };
-
-    
   return (
     <div>
-        <div className="flex flex-row items-center justify-center gap-4 py-8 md:py-10">
-            <Select label="Select Search Filter" placeholder="Select Search Filter" selectedKeys={[filter]} onChange={handleSelectionChange} className="w-1/6">
-                {filterOptions.map((option) => (
-                    <SelectItem key={option.value} value={option.value}>{option.label}</SelectItem>
-                ))}
-            </Select>
-            <Input placeholder="Search" label="Search" className="w-1/3"value={value} onValueChange={setValue}/>
-            <Button className="bg-blue-600" onPress={handleRequest}>Search</Button>
-        </div>
-        <div className="grid grid-cols-4 gap-12">
-            {animeListing.map((anime, index) => {return(<AnimeTiles key={index} title={anime.attributes.titles.en_us || anime.attributes.canonicalTitle} coverImage={anime.attributes.posterImage.medium} averageRating={anime.attributes.averageRating} tinyCoverImage={anime.attributes.posterImage.tiny} description={anime.attributes.description}/>)})}
-        </div>       
+      <div className="flex flex-row items-center justify-center gap-4 py-8 md:py-10">
+        <Select
+          label="Select Search Filter"
+          placeholder="Select Search Filter"
+          selectedKeys={[filter]}
+          onChange={handleSelectionChange}
+          className="w-1/6"
+        >
+          {filterOptions.map((option) => (
+            <SelectItem key={option.value} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </Select>
+        <Input
+          placeholder="Search"
+          label="Search"
+          className="w-1/3"
+          value={value}
+          onValueChange={setValue}
+        />
+        <Button className="bg-blue-600" onPress={handleRequest}>
+          Search
+        </Button>
+      </div>
+      <div className="grid grid-cols-4 gap-12">
+        {animeListing.map((anime, index) => {
+          return (
+            <AnimeTiles
+              key={index}
+              title={
+                anime.attributes.titles.en_us || anime.attributes.canonicalTitle
+              }
+              coverImage={anime.attributes.posterImage.medium}
+              averageRating={anime.attributes.averageRating}
+              tinyCoverImage={anime.attributes.posterImage.tiny}
+              description={anime.attributes.description}
+              ageRating={anime.attributes.ageRating}
+              episodeCount={anime.attributes.episodeCount}
+              nextRelease={anime.attributes.nextRelease}
+              youtubeVideoId={anime.attributes.youtubeVideoId}
+              subType={anime.attributes.subtype}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- components/anime-tiles.tsx
+++ components/anime-tiles.tsx
@@ -1,39 +1,83 @@
-import React from 'react'
-import {Card, CardFooter, CardHeader, Image, Modal,ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure} from "@nextui-org/react";
+import React from "react";
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  Image,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Button,
+  Link,
+} from "@nextui-org/react";
 
-const AnimeTiles = ({title, coverImage, averageRating, tinyCoverImage, description}) => {
-  const {isOpen, onOpen, onClose} = useDisclosure();
+const AnimeTiles = ({
+  title,
+  coverImage,
+  averageRating,
+  tinyCoverImage,
+  description,
+  ageRating,
+  episodeCount,
+  nextRelease,
+  youtubeVideoId,
+  subType,
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  
   return (
     <div>
       <Card isPressable onPress={onOpen}>
-        <Image src={coverImage} alt={title} className='z-1'/>
+        <Image src={coverImage} alt={title} className="z-1" />
         <CardFooter>
           <h1>{title}</h1>
         </CardFooter>
       </Card>
       <Modal isOpen={isOpen} onClose={onClose} size="5xl">
         <ModalContent>
-          {(onClose)=> (
-            <div className=''>
-              <ModalHeader className='grid grid-cols-3 gap-12 mx-4'>
-                <Image src={tinyCoverImage} alt={title} className='justify-self-start'/>
-                <h1 className='text-xl justify-self-center'>{title}</h1>
-                <h1 className='justify-self-end'>Rating: {averageRating}</h1>
+          {(onClose) => (
+            <div className="">
+              <ModalHeader className="grid grid-cols-3 gap-12 mx-4">
+                <Image
+                  src={tinyCoverImage}
+                  alt={title}
+                  className="justify-self-start"
+                />
+                <h1 className="text-xl justify-self-center">{title}</h1>
+                <h1 className="justify-self-end">Rating: {averageRating}</h1>
               </ModalHeader>
-              <ModalBody className=''>
-                <p>{description}</p>
+              <ModalBody>
+                <div className="grid grid-cols-3">
+                  <ul>
+                    <li>Age Rating: {ageRating}</li>
+                    <li>Episode Count: {episodeCount}</li>
+                    <li>Next Release: {nextRelease}</li>
+                    <li>Type: {subType}</li>
+                  </ul>
+                  <div className="justify-self-center">
+                    <p className="">Watch A Preview</p>
+                    <Button
+                      as={Link}
+                      isExternal
+                      href={`https://youtube.com/watch?v=${youtubeVideoId}`}
+                      className="w-1/4 bg-red-500"
+                    >
+                      YouTube
+                    </Button>
+                  </div>
+                  <div></div>
+                </div>
+                <p className="justify-self-start">{description}</p>
               </ModalBody>
-              <ModalFooter>
-                
-              </ModalFooter>
             </div>
           )}
         </ModalContent>
       </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default AnimeTiles
+export default AnimeTiles;

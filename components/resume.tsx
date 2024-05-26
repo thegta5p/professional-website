93e553eb70fb818d728684f04564fa5dea7ee8f9
Isaac Castro <icast065@ucr.edu>
2024-05-26 15:38:44 -0700

    formatted doc

--- components/resume.tsx
+++ components/resume.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Resume from "../public/Resume.pdf"
-import {Card, CardHeader, CardFooter, Button, Image} from "@nextui-org/react";
+import React from "react";
+import Resume from "../public/Resume.pdf";
+import { Card, CardHeader, CardFooter, Button, Image } from "@nextui-org/react";
 
 const resume = () => {
   return (
-    <div className='mt-12'> 
-        <h1 className='text-xl'>Here is a link to my resume:</h1>
-        <Button onPress={() => window.open(Resume)} className='mt-4 bg-blue-500'>Resume</Button>
+    <div className="mt-12">
+      <h1 className="text-xl">Here is a link to my resume:</h1>
+      <Button onPress={() => window.open(Resume)} className="mt-4 bg-blue-500">
+        Resume
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default resume
+export default resume;

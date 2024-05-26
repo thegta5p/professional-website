ac3a95d6a59752920207f82ef81bf1a2aa27a553
Isaac Castro <icast065@ucr.edu>
2024-05-26 15:37:50 -0700

    added in link to resume in about

---
+++ components/resume.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Resume from "../public/Resume.pdf"
+import {Card, CardHeader, CardFooter, Button, Image} from "@nextui-org/react";
+
+const resume = () => {
+  return (
+    <div className='mt-12'> 
+        <h1 className='text-xl'>Here is a link to my resume:</h1>
+        <Button onPress={() => window.open(Resume)} className='mt-4 bg-blue-500'>Resume</Button>
+    </div>
+  )
+}
+
+export default resume
--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const { type } = require('os');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/professional-website",
   output: "export",
   images: { unoptimized: true },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pdf$/i,
+      type: 'asset/resource',
+    })
+    return config
+  },
 };
 
 module.exports = nextConfig;
